a49458330b534a340d0d1022d287e8da27226448
ArtemDomnikoff <adomnikov3@gmail.com>
2021-12-30 13:44:16 +0300

    temporary commit

--- pendulum.py
+++ pendulum.py
@@ -11,17 +11,21 @@ def get_angle(ang, h, w_speed):
     return ang + h * w_speed
 
 
-def animate(line, x, y):
-    line.set_ydata(x,y)
+def animate_line(line, x, y):
+    line.set_data(x, y)
     return [line]
 
 
 fig = plt.figure(figsize=(7, 4))
 ax = fig.add_subplot()
-phasa = np.arange(0, np.pi, 0.1)
-x = np.linspace(np.pi/2, -np.pi/2, 100)
-
-line, = ax.plot(np.sin(x), np.cos(x))
+ang = np.pi / 2
+w_speed = 0
+l = 1
+h = np.arange(0, np.pi, 0.01)
+angl = get_angle(ang, h, angle_speed(w_speed, l, h, ang))
+x = np.cos(angl) * l
+y = np.sin(angl) * l
+line, = plt.plot(x, y)
 '''animation = FuncAnimation(fig, func=animate, frames=60,  fargs=(line, x), interval=30, blit=True, repeat=True)'''
 ax.grid()
 plt.show()
