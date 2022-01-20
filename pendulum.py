9eb0ff7243dc5b781393df3292e4c52670bd8c69
ArtemDomnikoff <adomnikov3@gmail.com>
2022-01-20 11:03:52 +0300

    Repair the animation.

--- pendulum.py
+++ pendulum.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.axis([-1, 1, -1, 1])
 
 def angle_speed(w_speed, l, h, ang, g=9.8):
     return w_speed - (g / l) * h * np.sin(ang)
@@ -11,22 +14,20 @@ def get_angle(ang, h, w_speed):
     return ang + h * w_speed
 
 
-def animate_line(line, x, y):
-    line.set_data(x, y)
-    return [line]
+def animate(i,line,x,y):
+    # i - это и есть Фреймс
+    angle = get_angle(i, h, angle_speed(w_speed, l, h, i))
+    x = np.cos(angle) * l
+    y = np.sin(angle) * l
+    line.set_data([0,x],[0,y])
+    return line,
 
-
-fig = plt.figure(figsize=(7, 4))
-ax = fig.add_subplot()
-ang = np.pi / 2
-w_speed = 0
+h = 0
 l = 1
-h = np.arange(0, np.pi, 0.01)
-angl = get_angle(ang, h, angle_speed(w_speed, l, h, ang))
-x = np.cos(angl) * l
-y = np.sin(angl) * l
-line, = plt.plot(x, y)
-'''animation = FuncAnimation(fig, func=animate, frames=60,  fargs=(line, x), interval=30, blit=True, repeat=True)'''
+w_speed = 0
+x = 0
+y = 0
+line, = ax.plot(x,y)
+animation = FuncAnimation(fig, animate, frames=np.arange(0, 2*np.pi, 0.1), fargs=(line, x, y), interval=30, blit=False)
 ax.grid()
 plt.show()
-'''animation'''
